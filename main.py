bffeebd4ceccc6c340eb9108fd969a7e3703cb84
EstebanUB <batista.esteban99@gmail.com>
2022-10-28 14:28:18 +0200

    prueba 0

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # This is a sample Python script.
+import cv2
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -9,8 +14,14 @@ def print_hi(name):
     print(f'A, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('tebi')
+    asset_path = './assets/img/'
+    image = cv2.imread(asset_path + 'valtest.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    RMatrix = cv2.cornerHarris(gray, 3, 3, 0.04)
+    plt.imshow(image, cmap='gray')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ src/aimbot/aimagent.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+import math
+
+def aim():
+
+    return
